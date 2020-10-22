da4f0ab3ef5e73474912bdc185f9110cca5336fd
CyriusSecond <71562936+CyriusSecond@users.noreply.github.com>
2020-10-23 00:57:54 +0200

    Fix count off by 1, add URL and SHA256 plots

--- plot.py
+++ plot.py
@@ -2,9 +2,29 @@ import matplotlib.pyplot as plt
 import json
 import dateutil.parser
 
+### IPv4 Emotet (TA542)
 save_json = json.load(open("plot_ips.json"))
 for e in save_json:
 	plt.plot(dateutil.parser.parse(e[0]), e[1], e[2])
 plt.ylabel("Amount of IPs added to AlienVault from TU Delft dataset (459 IPs)")
 plt.xlabel("x = added by an API, o = added manually\nred = epoch1, green = epoch2, blue = epoch3, black = partner01")
+plt.title("IPv4 Emotet (TA542)")
+plt.show()
+
+### SHA256 Emotet (TA542)
+save_json = json.load(open("plot_sha256.json"))
+for e in save_json:
+	plt.plot(dateutil.parser.parse(e[0]), e[1], e[2])
+plt.ylabel("Amount of SHA256 checksums added to AlienVault from TU Delft dataset (54 SHA256 checksums)")
+plt.xlabel("x = added by an API, o = added manually")
+plt.title("SHA256 Emotet (TA542)")
+plt.show()
+
+### URL Emotet (TA542)
+save_json = json.load(open("plot_url.json"))
+for e in save_json:
+	plt.plot(dateutil.parser.parse(e[0]), e[1], e[2])
+plt.ylabel("Amount of URLs added to AlienVault from TU Delft dataset (50 IPs)")
+plt.xlabel("x = added by an API, o = added manually")
+plt.title("URL Emotet (TA542)")
 plt.show()
